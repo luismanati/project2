5737dacb34fac0b86fe47cb3de749f3c9bf31f70
Luis Cascante <luisalonzocascante@gmail.com>
2018-09-07 16:18:25 -0600

    project 2 finished

--- modules/custom/hogwarts_forms/src/Plugin/Block/ReverseFormBlock.php
+++ modules/custom/hogwarts_forms/src/Plugin/Block/ReverseFormBlock.php
@@ -3,7 +3,8 @@
 namespace Drupal\hogwarts_forms\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-
+use Drupal\Core\Form\FormBuilder;
+use Drupal\hogwarts_forms\Form\StringReverseForm;
 /**
  * Provides a 'ReverseFormBlock' block.
  *
@@ -20,7 +21,10 @@ class ReverseFormBlock extends BlockBase {
   public function build() {
     $build = [];
     $build['reverse_form_block']['#markup'] = 'Implement ReverseFormBlock.';
-
+    
+    $form = \Drupal::formBuilder()->getForm('\Drupal\hogwarts_forms\Form\StringReverseForm');
+    $build['reverse_form_block']['form'] = $form;
+    
     return $build;
   }
 
